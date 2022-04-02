c89f5f37b8cf946bbe60617cc83e6406e6f8799d
Alexandre Freitas <xande_liborio@hotmail.com>
2022-04-02 18:13:04 -0300

    223. Inherited Widget na Prática

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/pages/counter_page.dart';
 import 'package:flutter_shop/pages/product_detail_page.dart';
 import 'package:flutter_shop/pages/products_overview_page.dart';
+import 'package:flutter_shop/providers/counter.dart';
 import 'package:flutter_shop/utils/app,_routes.dart';
 
 void main() {
@@ -13,26 +15,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = ThemeData();
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Shop',
-      theme: theme.copyWith(
-        colorScheme: theme.colorScheme.copyWith(
-          primary: Colors.amber,
-          secondary: Colors.deepOrange,
-        ),
-        textTheme: ThemeData.light().textTheme.copyWith(
-              headline1: const TextStyle(
-                fontSize: 20,
-                fontFamily: 'Lato',
-                color: Colors.black,
+    return CounterProvider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Shop',
+        theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+            primary: Colors.amber,
+            secondary: Colors.deepOrange,
+          ),
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline1: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Lato',
+                  color: Colors.black,
+                ),
               ),
-            ),
+        ),
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => const CounterPage(),
+        },
       ),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (ctx) => const ProductDetailPage(),
-      },
     );
   }
 }
---
+++ lib/pages/counter_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop/providers/counter.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  @override
+  Widget build(BuildContext context) {
+    final provider = CounterProvider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exemplo Contador'),
+      ),
+      body: Column(
+        children: [
+          Text(provider?.state.value.toString() ?? '0'),
+          IconButton(
+            onPressed: () {
+              setState(() => provider?.state.increment());
+            },
+            icon: Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() => provider?.state.decrement());
+            },
+            icon: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
