368be368e04be3f819f81aecf11646e8eb9f5979
Alexandre Freitas <xande_liborio@hotmail.com>
2022-04-04 19:30:47 -0300

    233. Filtrar Favoritos 01

--- lib/model/product_list.dart
+++ lib/model/product_list.dart
@@ -4,11 +4,27 @@ import 'package:flutter_shop/model/product.dart';
 
 class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
+  bool _showFavoriteOnly = false;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if (_showFavoriteOnly) {
+      return _items.where((element) => element.isFavorite).toList();
+    }
+    return [..._items];
+  }
 
   void addProduct(Product product) {
     _items.add(product);
+    notifyListeners();
+  }
+
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
     notifyListeners();
   }
 }
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/components/product_grid.dart';
+import 'package:flutter_shop/model/product_list.dart';
+import 'package:provider/provider.dart';
+
+enum FilterOptions {
+  FAVORITE,
+  ALL,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Minha Loja',
           style: TextStyle(fontFamily: 'Lato'),
         ),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.FAVORITE,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.ALL,
+              ),
+            ],
+            onSelected: (selectedValue) {
+              if (selectedValue == FilterOptions.FAVORITE) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          ),
+        ],
         centerTitle: true,
       ),
       body: ProductGrid(),
