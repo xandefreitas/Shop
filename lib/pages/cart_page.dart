d221eae4a26181a2c4e94d043f6dbe1ffa5a945c
Alexandre Freitas <xande_liborio@hotmail.com>
2022-04-06 09:40:10 -0300

    239. Componente Carrinho 01

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/model/cart.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Carrinho'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(24),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(width: 8),
+                  Chip(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    label: Text(
+                      cart.totalAmount.toStringAsFixed(2),
+                      style: TextStyle(
+                        color: Theme.of(context).primaryTextTheme.headline6?.color,
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      textStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+                    ),
+                    child: Text('Comprar'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
