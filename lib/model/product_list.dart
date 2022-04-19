93b7b41cbe22be463d05904bc457d62ab0d80ebe
Alexandre Freitas <xande_liborio@hotmail.com>
2022-04-19 12:41:32 -0300

    284. Correção no Carregamento dos Produtos

--- lib/model/product_list.dart
+++ lib/model/product_list.dart
@@ -39,6 +39,7 @@ class ProductList with ChangeNotifier {
   }
 
   Future<void> loadProducts() async {
+    _items.clear();
     final response = await http.get(Uri.parse(_url));
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
