9648bd9a1b1f836acfc73468784979317af16c03
Alexandre Freitas <xande_liborio@hotmail.com>
2022-05-13 15:23:32 -0300

    328. Animando a Tela de Pedidos

--- lib/components/order_widget.dart
+++ lib/components/order_widget.dart
@@ -15,28 +15,32 @@ class _OrderWidgetState extends State<OrderWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text(
-              'R\$ ${widget.order.total.toStringAsFixed(2)}',
+    final _itemsHeight = (widget.order.products.length * 24.0) + 8;
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded ? _itemsHeight + 80 : 80,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                'R\$ ${widget.order.total.toStringAsFixed(2)}',
+              ),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
-            ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
-              height: (widget.order.products.length * 24.0) + 8,
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              height: _expanded ? _itemsHeight : 0,
               padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
               child: ListView(
                   children: widget.order.products
@@ -55,7 +59,8 @@ class _OrderWidgetState extends State<OrderWidget> {
                           ))
                       .toList()),
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
